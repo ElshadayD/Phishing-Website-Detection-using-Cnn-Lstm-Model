67d867b91211b1c17a51175693674a87a066d0ce
Elshaday_Asfaw <71713154+ElshadayD@users.noreply.github.com>
2023-12-10 11:52:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,39 @@
+from flask import Flask, render_template, request, jsonify
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import scikitplot as skplt
-import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Embedding, LSTM, GlobalAveragePooling2D
-from tensorflow.keras.layers import Dense, Dropout, Conv1D, MaxPooling1D, BatchNormalization
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score,classification_report,confusion_matrix
+from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
 
-data = pd.read_csv('malicious_phish.csv')
+app = Flask(__name__)
 
+# Load the pre-trained model
+model = load_model('categorical_model.h5')
+
+# Load the label encoder and tokenizer
 le = LabelEncoder()
-data['type_encoded'] = le.fit_transform(data['type'])
+le.classes_ = np.load('label_encoder_classes.npy')
+tokenizer = Tokenizer()
+tokenizer.word_index = np.load('tokenizer_word_index.npy').item()
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-X_train, X_test, y_train, y_test = train_test_split(data['url'], data['type_encoded'], test_size=0.25, shuffle=True, random_state=42)
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        url = request.form['url']
+        # Tokenize and pad the input text
+        seq = tokenizer.texts_to_sequences([url])
+        padded = pad_sequences(seq, maxlen=100)
+        # Make prediction
+        prediction = np.argmax(model.predict(padded), axis=-1)[0]
+        # Convert prediction back to original label
+        predicted_label = le.inverse_transform([prediction])[0]
+        return render_template('result.html', url=url, predicted_label=predicted_label)
 
-
-tokenizer = Tokenizer(num_words=1000)
-tokenizer.fit_on_texts(X_train)
-X_train_seq = tokenizer.texts_to_sequences(X_train)
-X_test_seq = tokenizer.texts_to_sequences(X_test)
-vocab_size = len(tokenizer.word_index) + 1
-
-
-maxlen = 100 # or some other appropriate value
-X_train_padded = pad_sequences(X_train_seq, maxlen=maxlen)
-X_test_padded = pad_sequences(X_test_seq, maxlen=maxlen)
-
-model = Sequential(name="Cnn-Lstm_model")
-model.add(Embedding(input_dim=vocab_size, output_dim=8,
-input_length=maxlen, name='layer_embedding'))
-model.add(BatchNormalization())
-model.add(Conv1D(filters = 32, kernel_size = 9, padding = 'same', activation = 'relu'))
-model.add(MaxPooling1D(pool_size = 2))
-model.add(LSTM(units=512, return_sequences=False, dropout=0.2))
-model.add(Dense(units=4, activation='softmax'))
-model.summary()
-
-optimizer = Adam(learning_rate=.0001)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
-y_train_onehot = to_categorical(y_train)
-y_test_onehot = to_categorical(y_test)
-
-history = model.fit(X_train_padded, y_train_onehot, validation_split=0.2, epochs=150, batch_size=512)
-
-model.save('categorical_model.h5')
-
-y_pred = np.argmax(model.predict(X_test_padded), axis=-1)
-
-print(classification_report(y_test, y_pred))
-print(confusion_matrix(y_test, y_pred))
+if __name__ == '__main__':
+    app.run(debug=True)
